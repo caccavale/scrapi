6bb35da5f7ce412b457f1a252db954c8ecfcbf31
Caccavale <samcaccavale@gmail.com>
2018-07-22 13:59:50 -0400

    Made caching atomic

--- README.md
+++ README.md
@@ -4,12 +4,10 @@ ScrAPI is a really dump afternoon project that "wraps" any website in a _very_ d
 
 It is run with `python3 scrapi <website>` and spawns a flask instance on the default `127.0.0.1:5000`.  You can then query this endpoint like you would the website:
 
-`python3 scrapi github.com` queried at `curl 127.0.0.1:5000/caccavale?links=a&` -> would return a json `links` to all things with css selector `a` on the page `github.com/caccavale`.
-
-You might ask why this is useful.  It is not.
+`python3 scrapi github.com` queried with `curl 127.0.0.1:5000/caccavale?links=a&` -> would return a json with key `links` to all things with css selector `a` on the page `github.com/caccavale`.
 
 ### Advanced Usage...
-When I originally made this my roommate [Matt](https://github.com/mattrberry) said it had to be less that 50 lines.  As such, I've tried to fit as much _dumb_ functionality within those bounds.  If you use scrapi as a library, you can pass in your own cookies with by setting the optional kwarg `cookies` upon construction.  You can also enable caching with `cache=True` and change the cache size with `max_cache_size` (optimally a power of 2).
+When I originally made this my roommate [Matt](https://github.com/mattrberry) said it had to be less that ~~50~~ _cough_-- 60 lines.  As such, I've tried to fit as much _dumb_ functionality within those bounds.  If you use scrapi as a library, you can pass in your own cookies with by setting the optional kwarg `cookies` upon construction.  You can also enable caching with `cache=True` and change the cache size with `max_cache` (optimally a power of 2).
 
 ### Examples:
 ```
@@ -17,7 +15,15 @@ python3 scrapi caccavale.github.io &
 curl "127.0.0.1:5000/?links=a&"
 >> {"links": ["<a href=\"mailto:samcaccavale@gmail.com\">email</a>", "<a href=\"http://github.com/caccavale\">github</a>"]}
 ```
-
+As a module:
+```
+python3
+from scrapi import Scrapi
+Scrapi('caccavale.github.io',
+        cache=True,
+        max_cache=256,
+        cookies='favorite=chocolate_chip').run()
+```
 Note, these are equivalent:
 ```
 python scrapi github.com/caccavale &
--- scrapi/__init__.py
+++ scrapi/__init__.py
@@ -16,34 +16,43 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-class Scrapi(Flask):
-    def __init__(self, domain, cookies=None, cache=False, max_cache_size=128, *args, **kwargs):
-        if not domain.startswith('http'):
-            domain = 'http://' + domain
-        self.domain = domain
-        self.cookies = cookies if cookies is not None else {}
-        super().__init__(__name__, *args, **kwargs)
 
-        if cache:
-            self.got = functools.lru_cache(maxsize=max_cache_size)(self.got)
+class Scrapi(Flask):
+    def __init__(self, domain, cookies=None, cache=False, max_cache=128, *args, **kwargs):
+        super().__init__(__name__, *args, **kwargs)
+        self.domain = domain if domain.startswith('http') else 'http://' + domain
+        self.cookies = cookies if cookies is not None else {}
+        self.cache = cache
+        if self.cache:
+            for func in [self.cached, self.get_soup, self.get_page]:
+                setattr(self, func.__name__, functools.lru_cache(max_cache)(func))
         self.route('/<path:path>', methods=['GET'])(self.get)
         self.route('/', methods=['GET'])(self.get)
 
     def get(self, path=''):
-        return self.got(path, urllib.parse.unquote(request.query_string.decode()))
-
-    def got(self, path='', query_string=''):
-        page = requests.get('/'.join([self.domain, path]), cookies=self.cookies).text
+        query_string = urllib.parse.unquote(request.query_string.decode())
         if not query_string:
-            return page
+            return self.get_page(path)
 
-        soup = Soup(page, 'lxml')
-        results = {}
-        for key, selectors in urllib.parse.parse_qs(query_string).items():
-            for selector in selectors:
-                results[key] = results.get(key, []) + [str(tag) for tag in soup.select(selector)]
+        if self.cache is True:
+            selector_to_tags = lambda selector: self.cached(path, selector)
+        else:
+            soup = self.get_soup(path)
+            selector_to_tags = lambda selector: soup.select(selector)
 
-        return json.dumps(results)
+        return json.dumps({key: [str(tag) for selector in selectors
+                                 for tag in selector_to_tags(selector)] for
+                           key, selectors in urllib.parse.parse_qs(query_string).items()})
+
+    def cached(self, path, selector):
+        soup = self.get_soup(path)
+        return soup.select(selector)
+
+    def get_soup(self, path):
+        return Soup(self.get_page(path), 'lxml')
+
+    def get_page(self, path):
+        return requests.get('/'.join([self.domain, path]), cookies=self.cookies).text
 
 if __name__ == '__main__':
     import sys
